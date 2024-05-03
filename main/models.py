5122ca1b4c8fb7de6860a656c31e024dbc5edb55
DavlatovO <davlatovoybek007@gmail.com>
2024-05-03 05:44:17 +0500

    Changes have been made

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Region(models.Model):
+    name = models.CharField(max_length=100)
+    population = models.PositiveIntegerField()
+    area = models.FloatField()
+    picture = models.ImageField(upload_to='media', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from . import models
 
 def index(request):
+    regions = models.Region.objects.all() 
 
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'regions':regions})
